9e9418deece221da32f08ce1fc163aec23e89aac
nblack0917 <nickablack@gmail.com>
2021-03-14 18:02:36 -0500

    can drop specific item from list repeatedly

--- src/GridComp/GridComp.js
+++ src/GridComp/GridComp.js
@@ -29,43 +29,46 @@ class MyFirstGrid extends React.Component {
         // const layout = this.generateLayout();
         // this.state = { layoutList };
         this.state = { layoutList: [
-            {i: '0', x: 0, y: 0, w: 4, h: 4, minH: 2, maxH: 2, isResizable: false, isDraggable: true},
-            {i: '1', x: 6, y: 3, w: 2, h: 3, minH: 2, maxH: 2, isResizable: false, isDraggable: true},
-            {i: '2', x: 8, y: 0, w: 1, h: 1, minH: 2, maxH: 2, isResizable: false, isDraggable: true},
-            {i: '3', x: 8, y: 3, w: 3, h: 3, minH: 2, maxH: 2, isResizable: false, isDraggable: true},
-            {i: '4', x: 6, y: 0, w: 2, h: 2, minH: 2, maxH: 2, isResizable: false, isDraggable: true},
-            {i: '5', x: 0, y: 4, w: 2, h: 2, minH: 2, maxH: 2, isResizable: false, isDraggable: true},
-            {i: '6', x: 9, y: 0, w: 2, h: 2, minH: 2, maxH: 2, isResizable: false, isDraggable: true},
-            {i: '7', x: 3, y: 5, w: 1, h: 1, minH: 2, maxH: 1, isResizable: false, isDraggable: true},
+            {i: '0', x: 0, y: 0, w: 4, h: 4, minH: 2, maxH: 2, isResizable: false, isDraggable: true, isPlanter: false},
+            {i: '1', x: 6, y: 3, w: 2, h: 3, minH: 2, maxH: 2, isResizable: false, isDraggable: true, isPlanter: false},
+            {i: '2', x: 8, y: 0, w: 1, h: 1, minH: 2, maxH: 2, isResizable: false, isDraggable: true, isPlanter: true},
+            {i: '3', x: 8, y: 3, w: 3, h: 3, minH: 2, maxH: 2, isResizable: false, isDraggable: true, isPlanter: false},
+            {i: '4', x: 6, y: 0, w: 2, h: 2, minH: 2, maxH: 2, isResizable: false, isDraggable: true, isPlanter: true},
+            {i: '5', x: 0, y: 4, w: 2, h: 2, minH: 2, maxH: 2, isResizable: false, isDraggable: true, isPlanter: true},
+            {i: '6', x: 9, y: 0, w: 2, h: 2, minH: 2, maxH: 2, isResizable: false, isDraggable: true, isPlanter: true},
+            {i: '7', x: 3, y: 5, w: 1, h: 1, minH: 2, maxH: 1, isResizable: false, isDraggable: true, isPlanter: true},
             {i: '8', x: 12, y: 0, w: 1, h: 8, static: true},
           ],
         newSizes: [
-            {w: 2, h: 1},
-            {w: 3, h: 2},
-            {w: 2, h: 2},
-        ] };
+            {w: 2, h: 1, isPlanter: false},
+            {w: 3, h: 2, isPlanter: false},
+            {w: 2, h: 2, isPlanter: true},
+        ],
+        currentSize: {} };
       }
 
+      //builds list of items based on value of i
+      //also applies appropriate className depending on isPlanter boolean or lastItem
+      //lastItem is invisible barrier to create bounding effect for grid height
       generateDOM() {
         let lastItem = this.state.layoutList.length - 1
-        return _.map(_.range(itemNum), function(i) {
-            // console.log(i)
-            if ( i===2 || i === 4 || i === 5 || i === 6) {
+        let currentState = this.state.layoutList
+        return _.map(_.range(itemNum), function(iKey) {
+              if ( currentState[iKey].isPlanter) {
                 return (
-                    <div key={i} className="round">
-                      <span className="text">{i}</span>
+                    <div key={iKey} className="round">
+                      <span className="text">{iKey}</span>
                     </div>
                   );
-            } else if ( i===lastItem) {
+            } else if ( iKey===lastItem) {
                 return (
-                    <div key={i} className="lastElement">
-                      {/* <span className="text">{i}</span> */}
+                    <div key={iKey} className="lastElement">
                     </div>
                   );
             } else {
           return (
-            <div key={i} className="gridBox">
-              <span className="text">{i}</span>
+            <div key={iKey} className="gridBox">
+              <span className="text">{iKey}</span>
             </div>
           );
             }
@@ -91,20 +94,18 @@ class MyFirstGrid extends React.Component {
 
       onDrop = (layout, layoutItem, _event) => {
 
-        let newItemSize = this.state.newSizes[nextItemNum]
+        let newItemSize = this.state.currentSize
         console.log(newItemSize)
 
-        // console.log("layout before", layout)
-        console.log("layout Item", layoutItem)
         let stateList = this.state.layoutList
         //pull size and coords from dropped item
-        const itemX= layoutItem.x; const itemY= layoutItem.y; const itemW= newItemSize.w; const itemH= newItemSize.h;
-        // console.log(layoutItem)
+        const itemX= layoutItem.x; const itemY= layoutItem.y; const itemW= newItemSize.w; const itemH= newItemSize.h; const itemPlanter = newItemSize.isPlanter
+        console.log(layoutItem)
         const layoutLength = stateList.length
         const lastItem = parseInt(stateList[layoutLength - 1].i)
         const newIInt = lastItem+1;
         const newIString = newIInt.toString();
-        const newItem = {i: lastItem.toString(), x: itemX, y: itemY, w: itemW, h: itemH, minH: 1, maxH: 1, isResizable: false, isDraggable: true}
+        const newItem = {i: lastItem.toString(), x: itemX, y: itemY, w: itemW, h: itemH, minH: 1, maxH: 1, isResizable: false, isDraggable: true, isPlanter: itemPlanter}
         const newLastItem = {i: newIString, x: 12, y: 0, w: 1, h: 8, static: true}
   
         stateList.pop()
@@ -117,7 +118,16 @@ class MyFirstGrid extends React.Component {
         // console.log("layout after", layout)
       };
 
+      dragStart( index) {
+        //  console.log("index", index)
+         let currentItem = this.state.newSizes[index]
+         this.setState({ currentSize: currentItem})
+      }
+      drag(event) {
+        // event.dataTransfer.setData("text/plain", "")
+        // console.log(event)
 
+      }
 
   render() {
     // console.log("Starting layout", this.state.layoutList)
@@ -127,33 +137,58 @@ class MyFirstGrid extends React.Component {
     newLayout.x =+ 2
 
     return (
-            <div>
-                <div
-                    className="droppable-element"
-                    draggable={true}
-                    unselectable="on"
-                    // this is a hack for firefox
-                    // Firefox requires some kind of initialization
-                    // which we can do by adding this attribute
-                    // @see https://bugzilla.mozilla.org/show_bug.cgi?id=568313
-                    onDragStart={e => e.dataTransfer.setData("text/plain", "")}
-                    >
-                    Droppable Element (Drag me!)
+            <div className="container">
+                <div className="listContainer">
+                  {this.state.newSizes.map((item, index) => {
+                    //  console.log(index)
+                    let newWidth = item.w * 50;
+                    let newHeight = item.h * 50;
+                    let planterStyle = {};
+                    if (!item.isPlanter) {
+                      planterStyle = {
+                        width: newWidth,
+                        height: newHeight,
+                        borderRadius: 5,
+                      }
+                    } else {
+                      planterStyle = {
+                        width: newWidth,
+                        height: newHeight,
+                        borderRadius: '50%',
+                      }
+                    }
+                    return (
+                      <div
+                          className="droppable-element"
+                          draggable={true}
+                          unselectable="off"
+                          style={planterStyle}
+                          // this is a hack for firefox
+                          // Firefox requires some kind of initialization
+                          // which we can do by adding this attribute
+                          // @see https://bugzilla.mozilla.org/show_bug.cgi?id=568313
+                          onDragStart={() => this.dragStart(index)}
+                          // onDrag={this.drag}
+                          >
+                          {item.w} x {item.h}
+                      </div>
+                    )
+                  })}
                 </div>
-                        <div className="gridContainer">
-                <ReactGridLayout
-                
-                  layout={newLayout}
-                  onLayoutChange={this.onLayoutChange}
-                  onDragStop={this.onDragStop}
-                  isBounded={true}
-                  onDrop={this.onDrop}
-                  isDroppable={true}
-                  {...this.props}
-                >
+                <div className="gridContainer">
+                  <ReactGridLayout
+                  
+                    layout={newLayout}
+                    onLayoutChange={this.onLayoutChange}
+                    onDragStop={this.onDragStop}
+                    isBounded={true}
+                    onDrop={this.onDrop}
+                    isDroppable={true}
+                    {...this.props}
+                  >
                   {this.generateDOM()}
-                </ReactGridLayout>
-                        </div>
+                  </ReactGridLayout>
+                </div>
             </div>
       );
   }
