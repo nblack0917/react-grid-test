b867f90819cf2f336ee559177b0fdacfe4ed5acc
nblack0917 <nickablack@gmail.com>
2021-03-14 10:30:29 -0500

    can place items of various sizes from outside

--- src/GridComp/GridComp.js
+++ src/GridComp/GridComp.js
@@ -8,24 +8,14 @@ import './GridComp.css'
 const ReactGridLayout = WidthProvider(RGL);
 
 let itemNum = 9;
-
-// const layoutList = [
-//     {i: '0', x: 0, y: 0, w: 4, h: 4, minH: 2, maxH: 2, isResizable: false, isDraggable: true},
-//     {i: '1', x: 5, y: 3, w: 2, h: 4, minH: 2, maxH: 2, isResizable: false, isDraggable: true},
-//     {i: '2', x: 9, y: 0, w: 2, h: 3, minH: 2, maxH: 2, isResizable: false, isDraggable: true},
-//     {i: '3', x: 8, y: 3, w: 3, h: 3, minH: 2, maxH: 2, isResizable: false, isDraggable: true},
-//     {i: '4', x: 5, y: 0, w: 2, h: 3, minH: 2, maxH: 2, isResizable: false, isDraggable: true},
-//     {i: '5', x: 9, y: 9, w: 2, h: 3, minH: 2, maxH: 2, isResizable: false, isDraggable: true},
-//     {i: '6', x: 7, y: 0, w: 2, h: 3, minH: 2, maxH: 2, isResizable: false, isDraggable: true},
-//     {i: '7', x: 12, y: 0, w: 1, h: 13, static: true},
-//   ];
+let nextItemNum = 0;
 
 class MyFirstGrid extends React.Component {
     static defaultProps = {
         className: "layout",
         items: itemNum,
         cols: 12,
-        rowHeight: 27,
+        rowHeight: 47,
         width: 500,
         onLayoutChange: function() {},
         // This turns off compaction so you can place items wherever.
@@ -40,15 +30,20 @@ class MyFirstGrid extends React.Component {
         // this.state = { layoutList };
         this.state = { layoutList: [
             {i: '0', x: 0, y: 0, w: 4, h: 4, minH: 2, maxH: 2, isResizable: false, isDraggable: true},
-            {i: '1', x: 5, y: 3, w: 2, h: 4, minH: 2, maxH: 2, isResizable: false, isDraggable: true},
-            {i: '2', x: 9, y: 0, w: 2, h: 3, minH: 2, maxH: 2, isResizable: false, isDraggable: true},
+            {i: '1', x: 6, y: 3, w: 2, h: 3, minH: 2, maxH: 2, isResizable: false, isDraggable: true},
+            {i: '2', x: 8, y: 0, w: 1, h: 1, minH: 2, maxH: 2, isResizable: false, isDraggable: true},
             {i: '3', x: 8, y: 3, w: 3, h: 3, minH: 2, maxH: 2, isResizable: false, isDraggable: true},
-            {i: '4', x: 5, y: 0, w: 2, h: 3, minH: 2, maxH: 2, isResizable: false, isDraggable: true},
-            {i: '5', x: 9, y: 9, w: 2, h: 3, minH: 2, maxH: 2, isResizable: false, isDraggable: true},
-            {i: '6', x: 7, y: 0, w: 2, h: 3, minH: 2, maxH: 2, isResizable: false, isDraggable: true},
-            {i: '7', x: 2, y: 8, w: 1, h: 1, minH: 2, maxH: 1, isResizable: false, isDraggable: true},
-            {i: '8', x: 12, y: 0, w: 1, h: 13, static: true},
-          ] };
+            {i: '4', x: 6, y: 0, w: 2, h: 2, minH: 2, maxH: 2, isResizable: false, isDraggable: true},
+            {i: '5', x: 0, y: 4, w: 2, h: 2, minH: 2, maxH: 2, isResizable: false, isDraggable: true},
+            {i: '6', x: 9, y: 0, w: 2, h: 2, minH: 2, maxH: 2, isResizable: false, isDraggable: true},
+            {i: '7', x: 3, y: 5, w: 1, h: 1, minH: 2, maxH: 1, isResizable: false, isDraggable: true},
+            {i: '8', x: 12, y: 0, w: 1, h: 8, static: true},
+          ],
+        newSizes: [
+            {w: 2, h: 1},
+            {w: 3, h: 2},
+            {w: 2, h: 2},
+        ] };
       }
 
       generateDOM() {
@@ -77,23 +72,6 @@ class MyFirstGrid extends React.Component {
         });
       }
 
-    //   generateLayout() {
-    //     const p = this.props;
-    //     return _.map(new Array(p.items), function(item, i) {
-    //       const y = _.result(p, "y") || Math.ceil(Math.random() * 4) + 1;
-    //       return {
-    //         x: (i * 2) % 12,
-    //         y: Math.floor(i / 6) * y,
-    //         w: y,
-    //         h: y,
-    //         i: i.toString(),
-    //         isResizable: false,
-    //         minH: 1,
-    //         maxH: 1,
-    //       };
-    //     });
-    //   }
-
       onLayoutChange(layout) {
         this.props.onLayoutChange(layout);
       }
@@ -101,52 +79,53 @@ class MyFirstGrid extends React.Component {
       onDragStop(layout) {
           console.log(layout)
       }
+
+      //When new item is dropped from outside...
+      //get copy of layout state
+      //find index and i value of last item
+      //create new item based on size and coords of new item with i value of removed item
+      //create new last item with i value increased
+      //remove last item
+      //add new items then add new last item to state copy
+      //set state to state copy
+
       onDrop = (layout, layoutItem, _event) => {
-        // alert(`Dropped element props:\n${JSON.stringify(layoutItem, ['x', 'y', 'w', 'h'], 2)}`);
-        console.log("layout before", layout)
+
+        let newItemSize = this.state.newSizes[nextItemNum]
+        console.log(newItemSize)
+
+        // console.log("layout before", layout)
+        console.log("layout Item", layoutItem)
         let stateList = this.state.layoutList
-        const itemX= layoutItem.x; const itemY= layoutItem.y; const itemW= layoutItem.w; const itemH= layoutItem.h;
-        console.log(layoutItem)
+        //pull size and coords from dropped item
+        const itemX= layoutItem.x; const itemY= layoutItem.y; const itemW= newItemSize.w; const itemH= newItemSize.h;
+        // console.log(layoutItem)
         const layoutLength = stateList.length
         const lastItem = parseInt(stateList[layoutLength - 1].i)
-        // const lastItem = layoutList[layoutLength - 1]
         const newIInt = lastItem+1;
         const newIString = newIInt.toString();
         const newItem = {i: lastItem.toString(), x: itemX, y: itemY, w: itemW, h: itemH, minH: 1, maxH: 1, isResizable: false, isDraggable: true}
-        const newLastItem = {i: newIString, x: 12, y: 0, w: 1, h: 13, static: true}
-        // for (let item in layoutList) {
-        //     if (parseInt(layoutList[item].i) === lastItem) {
-        //         layoutList[item] = {i: newIString, x: 12, y: 0, w: 1, h: 13, static: true}
-        //         break;
-        //     }
-        // }
-        layout.pop()
+        const newLastItem = {i: newIString, x: 12, y: 0, w: 1, h: 8, static: true}
+  
+        stateList.pop()
         itemNum++;
-        // this.state.layout.items++
-        layout.push(newItem)
-        layout.push(newLastItem)
-        console.log("itemX", itemX)
-        console.log("stateList", stateList)
+        nextItemNum++;
+        stateList.push(newItem);
+        stateList.push(newLastItem)
+        // console.log("stateList", stateList)
         this.setState({ layoutList: stateList})
-        console.log("layout after", layout)
-        // console.log("lastItem", lastItem)
-        // console.log("newIString", newIString)
-        // console.log("itemNum", itemNum)
-        // console.log("layout Item", this.state.layoutItem)
-        // console.log("_event", _event)
-        // this.onLayoutChange(this.state.layoutList)
-
+        // console.log("layout after", layout)
       };
 
-      componentDidUpdate(layout) {
-          this.generateDOM()
-          this.onLayoutChange(layout)
-        //   console.log(this.state.layoutList)
-      }
+
 
   render() {
-    // layout is an array of objects, see the demo for more complete usage
-    // console.log(this.state.layoutList)
+    // console.log("Starting layout", this.state.layoutList)
+
+    //create copy of state to assist in refresh of component
+    let newLayout = JSON.parse(JSON.stringify(this.state.layoutList))
+    newLayout.x =+ 2
+
     return (
             <div>
                 <div
@@ -164,7 +143,7 @@ class MyFirstGrid extends React.Component {
                         <div className="gridContainer">
                 <ReactGridLayout
                 
-                  layout={this.state.layoutList}
+                  layout={newLayout}
                   onLayoutChange={this.onLayoutChange}
                   onDragStop={this.onDragStop}
                   isBounded={true}
